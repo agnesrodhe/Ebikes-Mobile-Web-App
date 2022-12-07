2418a401869eebbfb28724b049e757f9033acc14
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 11:30:21 +0100

    Pass user to FirstScreen an MapView

--- src/tabs/findscooter/index.js
+++ src/tabs/findscooter/index.js
@@ -2,23 +2,34 @@ import React, { useState } from 'react';
 import FirstScreen from './screens/firstscreen.js';
 import MapView from './screens/mapview.js';
 
-function FindScooters({ setTab }) {
-  const [screen, setScreen] = useState("firstscreen");
-  const [city, setCity] = useState("");
-  const [coordinates, setCoordinates] = useState("");
-return (
-  <div>
+function FindScooters({ user, setTab }) {
+    const [screen, setScreen] = useState("firstscreen");
+    const [city, setCity] = useState("");
+    const [coordinates, setCoordinates] = useState("");
 
-    {screen === "firstscreen" &&
-      <FirstScreen setScreen={setScreen} setCity={setCity} setCoordinates={setCoordinates} />
-    }
+    return (
+        <div>
 
-    {screen === "map" &&
-      <MapView setScreen={setScreen} city={city} coordinates={coordinates} setTab={setTab} />
-    }
-  </div>
+            {screen === "firstscreen" &&
+                <FirstScreen
+                    user={user}
+                    setScreen={setScreen}
+                    setCity={setCity}
+                    setCoordinates={setCoordinates}
+                />
+            }
 
-  )
-};
+            {screen === "map" &&
+                <MapView
+                    user={user}
+                    setScreen={setScreen}
+                    city={city}
+                    coordinates={coordinates}
+                    setTab={setTab}
+                />
+            }
+        </div>
+    );
+}
 
 export default FindScooters;
