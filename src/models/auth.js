df6ca479647543b4768f93cb8fc4bc04382dfda1
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 10:39:36 +0100

    Add signUp and signIn

---
+++ src/models/auth.js
@@ -0,0 +1,45 @@
+// Manage login
+
+const baseURL = "http://localhost:3002";
+
+const authModel = {
+    signIn: async function signIn(user) {
+        const result = fetch(`${baseURL}/v1/user/signin`, {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        return result;
+    },
+
+    signUp: async function signUp(user) {
+        const result = fetch(`${baseURL}/v1/user/signup`, {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        return result;
+    }
+};
+
+export default authModel;
