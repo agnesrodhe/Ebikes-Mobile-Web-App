d910510dd23ed138d4d5474d22eb742af339107a
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 11:13:57 +0100

    Add components ShowTrip, TripEnded and NoTrip

--- src/tabs/mytrip/index.js
+++ src/tabs/mytrip/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import FirstScreen from './FirstScreen';
+import React, { useEffect, useState } from 'react';
+import NoTrip from './notrip';
+import ShowTrip from './showtrip';
+import TripEnded from './tripended';
 import '../../style/mypagetab.css';
 import '../../style/buttons.css';
 
-function MyTrip() {
+function MyTrip({ user, setUser }) {
+    const [onGoingTrip, setOnGoingTrip] = useState(false);
+    const [showReciept, setShowReciept] = useState(false);
+    const [tripInfo, setTripInfo] = useState({});
+
+    useEffect(() => {
+        const currentTrip = localStorage.getItem('trip');
+
+        if (currentTrip) {
+            setOnGoingTrip(true);
+        }
+    }, []);
+
     return (
-        <FirstScreen />
+        <>
+            {onGoingTrip &&
+                <ShowTrip
+                    user={user}
+                    setUser={setUser}
+                    setOnGoingTrip={setOnGoingTrip}
+                    setShowReciept={setShowReciept}
+                    setTripInfo={setTripInfo}
+                />
+            }
+
+            {!onGoingTrip &&
+                <>
+                    {showReciept
+                        ? <TripEnded tripInfo={tripInfo} />
+                        : <NoTrip />
+                    }
+                </>
+            }
+        </>
     );
 }
 
