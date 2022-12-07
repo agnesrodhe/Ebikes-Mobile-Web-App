097d8a343165522b4d3f8214ef6a396a6bb943a1
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 11:31:22 +0100

    Pass user to components

--- src/tabnavigation/navigation.js
+++ src/tabnavigation/navigation.js
@@ -10,14 +10,14 @@ import FindScooters from '../tabs/findscooter/index.js';
 import MyTrip from '../tabs/mytrip/index.js';
 import MyPage from '../tabs/mypage/index.js';
 
-function Navigation({ setIsLoggedIn }) {
-    const [tab, setTab] = useState(0);
+function Navigation({ test, user, setUser }) {
+    const [tab, setTab] = useState(test || 0);
 
     return (
         <div style={{ height: '100vh' }}>
-            {tab === 0 && <FindScooters setTab={setTab} />}
-            {tab === 1 && <MyTrip />}
-            {tab === 2 && <MyPage setIsLoggedIn={setIsLoggedIn} />}
+            {tab === 0 && <FindScooters user={user} setTab={setTab} />}
+            {tab === 1 && <MyTrip user={user} setUser={setUser} />}
+            {tab === 2 && <MyPage user={user} setUser={setUser} />}
             <BottomNavigation
                 sx={{ width: '100%', position: 'absolute', bottom: 0, color: 'green'}}
                 showLabels
