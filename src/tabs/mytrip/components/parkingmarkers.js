571854da9ef7ae353cf5dc35fbfef0abdf5d9639
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 11:08:21 +0100

    Component show parking zones on map

---
+++ src/tabs/mytrip/components/parkingmarkers.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MarkerF } from '@react-google-maps/api';
+import parking from "../../../assets/parking.png";
+
+function ParkingMarkers({ parkingZones }) {
+    const allZonesMaped = parkingZones.map((zone, index) => {
+        return <MarkerF
+            key={index}
+            icon={parking}
+            position={{
+                lat: zone.location.coordinates[1],
+                lng: zone.location.coordinates[0]
+            }}
+        />;
+    });
+
+    return allZonesMaped;
+}
+
+export default ParkingMarkers;
