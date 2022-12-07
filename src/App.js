f614b428e51660c025a635eb46ded2b2aae36f3a
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 10:47:59 +0100

    Add check if logged in via backend route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "../src/style/loading.css";
 import Navigation from './tabnavigation/navigation.js';
 import Index from './login/index.js';
+import userModel from './models/user.js';
 
-function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("loggedIn") || false);
+function App({ test }) {
+    const [user, setUser] = useState(test || "loading");
+
+    useEffect(() => {
+        (async () => {
+            if (!test) {
+                const usr = await axios
+                    .get('http://localhost:3002/v1/user/githubtoken', {
+                        withCredentials: true
+                    })
+                    .then((res) => res.data);
+
+                if (!usr) {
+                    setUser(null);
+                    return;
+                }
+                const fullUser = await userModel.getUser(usr.id || usr._id);
+
+                setUser(fullUser);
+            }
+        })();
+    }, []);
 
     return (
-        <div>
-            {isLoggedIn && <Navigation setIsLoggedIn={setIsLoggedIn}/>}
-            {!isLoggedIn && <Index setIsLoggedIn={setIsLoggedIn}/>}
-        </div>
+        <>
+            {user === "loading" ?
+                <div className="spinner-container-center">
+                    <div className="loading-spinner"></div>
+                </div>
+                :
+                <div>
+                    {user && <Navigation user={user} setUser={setUser}/>}
+                    {!user && <Index setUser={setUser}/>}
+                </div>
+            }
+        </>
     );
 }
 
