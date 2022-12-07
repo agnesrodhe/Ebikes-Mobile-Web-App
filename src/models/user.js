4ec62174edb94c4186558613d0b3286921570dc8
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-07 10:43:29 +0100

    Add updateUser

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+// Manage user
+
+const baseURL = "http://localhost:3002";
+
+const userModel = {
+    getUser: async function getUser(userId) {
+        const result = fetch(`${baseURL}/v1/customers/${userId}`, {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        return result;
+    },
+    updateUser: async function updateUser(userId, body) {
+        const result = fetch(`${baseURL}/v1/user/${userId}`, {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(body),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        return result;
+    }
+};
+
+export default userModel;
