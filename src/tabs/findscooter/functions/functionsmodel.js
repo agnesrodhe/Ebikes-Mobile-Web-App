591129e575f0e87659213617677058bb6572a866
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-19 15:18:08 +0100

    Add getDistance to calulate distance between two coordinates

---
+++ src/tabs/findscooter/functions/functionsmodel.js
@@ -0,0 +1,28 @@
+// Findscooter functions
+import { getDistance } from 'geolib';
+
+const functionsModel = {
+    getDistance: function GetDistance(bike, parkingZones) {
+        // If in parkingzone
+        let inParkingZone = false;
+
+        parkingZones.forEach(zone => {
+            const distance = getDistance(
+                bike.location.coordinates,
+                {
+                    latitude: zone.location.coordinates[1],
+                    longitude: zone.location.coordinates[0]
+                }
+            );
+
+            // If bike is within 700 meters of parkingzone center
+            if (distance <= 700) {
+                inParkingZone = true;
+            }
+        });
+
+        return inParkingZone;
+    }
+};
+
+export default functionsModel;
