513ec1e0f57d811c78c7f878e9316c174484a0eb
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-12-19 15:04:15 +0100

    Get pricelist

--- src/tabnavigation/navigation.js
+++ src/tabnavigation/navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/tabnav.css';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 
@@ -9,14 +9,24 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import FindScooters from '../tabs/findscooter/index.js';
 import MyTrip from '../tabs/mytrip/index.js';
 import MyPage from '../tabs/mypage/index.js';
+import priceModel from '../models/prices';
 
 function Navigation({ test, user, setUser }) {
     const [tab, setTab] = useState(test || 0);
+    const [priceList, setPriceList] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const prices = await priceModel.getPriceList();
+
+            setPriceList(prices[0]);
+        })();
+    }, []);
 
     return (
         <div style={{ height: '100vh' }}>
-            {tab === 0 && <FindScooters user={user} setTab={setTab} />}
-            {tab === 1 && <MyTrip user={user} setUser={setUser} />}
+            {tab === 0 && <FindScooters user={user} priceList={priceList} setTab={setTab} />}
+            {tab === 1 && <MyTrip user={user} priceList={priceList} setUser={setUser} />}
             {tab === 2 && <MyPage user={user} setUser={setUser} />}
             <BottomNavigation
                 sx={{ width: '100%', position: 'absolute', bottom: 0, color: 'green'}}
