6bdab4704862983c93d8dc5f882c91b2220172e3
Vincent Wistrand <vincentwistrand@hotmail.se>
2023-01-01 13:59:41 +0100

    Add test

--- src/test/login/login.test.js
+++ src/test/login/login.test.js
@@ -1,23 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Login from '../../login/login.js';
-import App from '../../App.js';
 
-test('Login button exists', () => {
-    render(<Login />);
-    const loginButton = screen.getByRole("button", { name: /Logga in/i })
-    expect(loginButton).toBeInTheDocument();
-});
+describe('Login component', () => {
+    const setDisplay = jest.fn();
+    const setUser = jest.fn();
+  
+    beforeEach(() => {
+      render(<Login setDisplay={setDisplay} setUser={setUser} />);
+    });
 
-test('Login title', () => {
-    render(<Login />);
-    const logintitle = screen.getAllByRole("heading", { name: /Logga in/i })
-    expect(logintitle[0]).toBeInTheDocument();
-});
+    test('Login button exists', () => {
+        const loginButton = screen.getByRole("button", { name: /Logga in/i })
+        expect(loginButton).toBeInTheDocument();
+    });
 
-test('Username and password fields should exist', () => {
-    render(<Login />);
-    const userField = screen.getByRole("textbox", { type: /email/i })
-    const passwordField = screen.getByRole("textbox", { type: /password/i })
-    expect(userField).toBeInTheDocument();
-    expect(passwordField).toBeInTheDocument();
+    test('Login title', () => {
+        const logintitle = screen.getAllByRole("heading", { name: /Logga in/i })
+        expect(logintitle[0]).toBeInTheDocument();
+    });
+
+    test('Username and password fields should exist', () => {
+        const userField = screen.getByRole("textbox", { type: /email/i })
+        const passwordField = screen.getByRole("textbox", { type: /password/i })
+        expect(userField).toBeInTheDocument();
+        expect(passwordField).toBeInTheDocument();
+    });
+
+    test('displays error message when login button is clicked with empty fields', () => {
+        const loginButton = screen.getByRole("button", { name: /Logga in/i });
+        fireEvent.click(loginButton);
+        expect(screen.getByTestId('error')).toHaveTextContent('Fyll i alla fält!');
+    });
 });
